9d226cac2e709655f3b7c292740fa50ef9bc5ea4
Julian Loaiza <julian.loaizal@udea.edu.co>
2018-09-21 22:37:17 -0500

    Primera versión

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RaceOperator } from 'rxjs/internal/observable/race';
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
+})
+export class ProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
